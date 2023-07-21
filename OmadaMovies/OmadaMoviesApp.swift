5ca646c77cd0a1a769b4ef844960e07f83b98458
Jarod Luebbert <jarodluebbert@gmail.com>
2023-07-21 16:06:27 -0700

    Initial Commit

---
+++ OmadaMovies/OmadaMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmadaMoviesApp.swift
+//  OmadaMovies
+//
+//  Created by Jarod Luebbert on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct OmadaMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
